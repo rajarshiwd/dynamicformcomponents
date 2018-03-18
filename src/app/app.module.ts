bfe3c02eccebb8496f4623e674b643e06a19b813
rajarshiwd <rajarshiwd.com>
2018-03-18 13:55:32 -0400

    module seperated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { DynamicFormModule } from './dynamic-form/dynamic-form.module';
 
 @NgModule({
-  declarations: [
+  imports: [
+    BrowserModule,
+    DynamicFormModule
+  ],
+  bootstrap: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
